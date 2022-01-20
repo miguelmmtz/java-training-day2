dacda5654d0b9188f425898ca492ec0d89fdfcc7
Miguel M <miguelmmtz9925@gmail.com>
2022-01-20 15:43:36 -0600

    Exercise completed

--- SwordProblem.java
+++ SwordProblem.java
@@ -2,17 +2,39 @@ import java.util.LinkedList;
 
 public class SwordProblem {
 	public static void main(String[] args) {
-		findAliveSoldier(5,1);
+		findkillerSoldier(100,1);
 	}
-	public static void findAliveSoldier(int soldiers, int start) {
+	public static void findkillerSoldier(int soldiers, int start) {
 		LinkedList<Integer> circle = new LinkedList<Integer>();
 		for(int i=1; i<=soldiers; i++) {
 			circle.add(i);
 		}
 		
-		for(int i= start-1; i < circle.size(); i++) {
-			System.out.println(circle.get(i)+" vivo");
-			circle.remove(i+1);
+		/*
+		 * int killerSoldier represents the soldier who has the sword
+		 */
+		
+		int killerSoldier;
+		
+		for(int i= start-1; i < circle.size() && circle.size() > 1; i++) {
+			killerSoldier = circle.get(i);
+			System.out.print("Soldier "+killerSoldier+" has the sword, ");
+			if(killerSoldier==circle.getLast()) {
+				System.out.println("soldier "+circle.getFirst()+" will die");
+				circle.removeFirst();
+//				System.out.println(circle);
+				i=-1;
+			} else if(circle.get(i+1)==circle.getLast()){
+				System.out.println("soldier "+circle.get(i+1)+" will die");
+				circle.remove(i+1);
+//				System.out.println(circle);
+				i=-1;
+			} else {
+				System.out.println("soldier "+circle.get(i+1)+" will die");
+				circle.remove(i+1);
+//				System.out.println(circle);
+			}
 		}
+		System.out.println("\n\nSoldier "+circle+" wont die");
 	}
 }
